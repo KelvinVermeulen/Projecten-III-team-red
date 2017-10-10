310c6fd754317d7c8277729e0b303a646d8eb3eb
Jens De Meyst <jens.demeyst.v3917@student.hogent.be>
2017-10-10 15:41:43 +0200

    updated Script

--- Microsoft exchange/Scripts/Installatiescript_JDM.ps1
+++ Microsoft exchange/Scripts/Installatiescript_JDM.ps1
@@ -1,26 +1,40 @@
-
 ECHO "Installing OS roles and features"
 
-Install-WindowsFeature AS-HTTP-Activation, Desktop-Experience, NET-Framework-45-Features,
- RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, RSAT-Clustering-Mgmt,
- RSAT-Clustering-PowerShell, Web-Mgmt-Console, WAS-Process-Model, Web-Asp-Net45,
+Install-WindowsFeature AS-HTTP-Activation, Desktop-Experience, 
+ RPC-over-HTTP-proxy, Web-Mgmt-Console, WAS-Process-Model, Web-Asp-Net45,
  Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression,
  Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext,
  Web-ISAPI-Filter, Web-Lgcy-Mgmt-Console, Web-Metabase, Web-Mgmt-Console,
  Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression,
- Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation, RSAT-ADDS
+ Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation
 ECHO "Done installing roles en features"
+
+ECHO "Installing NET-Framework-45-Features"
+Install-WindowsFeature NET-Framework-45-Features
+ECHO "Done installing NET-Framework-45-Features"
 
 ECHO "Installing Pre-Requisites"
 Install-WindowsFeature ADLDS
 ECHO "done installing ADLDS"
 
 ECHO "Exchange Management Shell opt"
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementScriptingTools,IIS-ManagementScriptingTools,IIS-IIS6ManagementCompatibility,IIS-LegacySnapIn,IIS-ManagementConsole,IIS-Metabase,IIS-WebServerManagementTools,IIS-WebServerRole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementScriptingTools,
+IIS-ManagementScriptingTools,IIS-IIS6ManagementCompatibility,IIS-LegacySnapIn,IIS-ManagementConsole,
+IIS-Metabase,IIS-WebServerManagementTools,IIS-WebServerRole
 ECHO "done installing EMS"
 
 ECHO "RSAT-ADDS feature for AD"
-Install-WindowsFeature RSAT-ADDS
+Install-WindowsFeature RSAT-ADDS, RSAT-Clustering, RSAT-Clustering-CmdInterface, RSAT-Clustering-Mgmt,
+ RSAT-Clustering-PowerShell,
 ECHO "done installing RSAT-ADDS"
+
+Write-Host "Press any key to restart"
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Write-Host
+Write-Host "A"
+Write-Host "B"
+Write-Host "C"
 
 Restart-Computer
