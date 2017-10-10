9dd7b179cfbdcf81e3b0e09bd9e57df0c6a04541
Jens De Meyst <jens.demeyst.v3917@student.hogent.be>
2017-10-10 16:17:51 +0200

    Added script for Active Directory

--- Microsoft exchange/Scripts/Voor Active Directory.ps1
+++ Microsoft exchange/Scripts/Voor Active Directory.ps1
@@ -1,6 +1,9 @@
 <#
 .Net Framework 4.5.2 is installed
+
 RSAT-ADDS is installed (installed by default)
+
+The forest functional level must be at least Windows Server 2008
 #>
 
 install-WindowsFeature RSAT-ADDS
