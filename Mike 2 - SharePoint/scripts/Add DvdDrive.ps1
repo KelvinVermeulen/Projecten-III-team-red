4dbf3ab82ca0945845be2d3d336ae7aff91247a2
RobinDHondt <robin.dhondt.u8567@student.hogent.be>
2017-10-21 09:41:44 +0200

    Werken aan scripts

--- Mike 2 - SharePoint/scripts/Add DvdDrive.ps1
+++ Mike 2 - SharePoint/scripts/Add DvdDrive.ps1
@@ -12,6 +12,9 @@ Set-VMDvdDrive -VMName WinSer16 -ControllerLocation 0 -ControllerNumber 1 -Path 
 Write-Host 'Windows SharePoint Server 2016 ISO Added'
 Write-Host 'Done'
 Write-Host 'Changing Location'
-Set-Location 'C:\Users\Robin Cleyman\Documents\HoGent\AJ3S1\Projecten III - Systeembeheer\git\p3ops-red\Mike 2 - SharePoint'
+#Robin cleyman
+#Set-Location 'C:\Users\Robin Cleyman\Documents\HoGent\AJ3S1\Projecten III - Systeembeheer\git\p3ops-red\Mike 2 - SharePoint'
+#Robin D'Hondt
+Set-Location 'C:\Users\marev\Desktop\School\Projecten III\p3ops-red\Mike 2 - SharePoint'
 Write-Host 'Ready for vagrant up'
 Start-VM -Name WinSer16
