de8a5c69b2d93f819a61e54eb31ecaddb6bd0bf1
Hilmi Emre Bayat <hilmi_emre@hotmail.com>
2017-10-22 14:38:11 +0200

    Update

---
+++ Kilo 2 - DHCP/Script/Naam en domein.ps1
@@ -0,0 +1,3 @@
+﻿$cred = New-Object System.Management.Automation.PSCredential("red.local\Administrator",(ConvertTo-SecureString "Aa12345" -AsPlainText -Force))
+Add-Computer -DomainName red.local -Credential $cred -Restart
+Rename-Computer -NewName DHCPServer -Restart
