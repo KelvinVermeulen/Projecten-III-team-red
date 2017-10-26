cfb835bf583eb6f487873ee0c200dbc660665546
RobinCleyman <keeper-robin@hotmail.com>
2017-10-26 15:21:17 +0200

    Script up to date gebracht

--- Mike 2 - SharePoint/scripts/3_installprerequisites.ps1
+++ Mike 2 - SharePoint/scripts/3_installprerequisites.ps1
@@ -9,13 +9,5 @@ $PreRequsInstallerPath= "E:"
 #Directory path where SP 2016 Pre-requisites files are kept 
 $PreRequsFilesPath = "C:\SharePointInstall\SharePoint_Prerequisites\PrerequisiteInstaller\" 
  
-Start-Process "$PreRequsInstallerPath\PrerequisiteInstaller.exe" -Wait -ArgumentList "  `  
-              /idfx11:`"$PreRequsFilesPath\MicrosoftIdentityExtensions-64.msi`" ` 
-              /Sync:`"$PreRequsFilesPath\Synchronization.msi`" `                                                                                  
-              /AppFabric:`"$PreRequsFilesPath\WindowsServerAppFabricSetup_x64.exe`" ` 
-              /kb3092423:`"$PreRequsFilesPath\AppFabric-KB3092423-x64-ENU.exe`" ` 
-              /MSIPCClient:`"$PreRequsFilesPath\setup_msipc_x64.exe`" `
-              /msvcrt11:`"$PreRequsFilesPath\vc_redist.x64.exe`" ` 
-              /msvcrt14:`"$PreRequsFilesPath\vcredist_x64.exe`" ` 
-              /dotnetfx:`"$PreRequsFilesPath\NDP47-KB3186497-x86-x64-AllOS-ENU.exe`""
+Start-Process "$PreRequsInstallerPath\PrerequisiteInstaller.exe" -Wait -ArgumentList /unattended,/idfx11:$PreRequsFilesPath\MicrosoftIdentityExtensions-64.msi,/Sync:$PreRequsFilesPath\Synchronization.msi,/AppFabric:$PreRequsFilesPath\WindowsServerAppFabricSetup_x64.exe,/kb3092423:$PreRequsFilesPath\AppFabric-KB3092423-x64-ENU.exe,/MSIPCClient:$PreRequsFilesPath\setup_msipc_x64.exe,/msvcrt11:$PreRequsFilesPath\vc_redist.x64.exe,/msvcrt14:$PreRequsFilesPath\vcredist_x64.exe,/dotnetfx:$PreRequsFilesPath\NDP47-KB3186497-x86-x64-AllOS-ENU.exe
 
