3781f8ccdd0cfea025087ef3dd277dca95b2661c
Benjamin <benjamin.vanlierde.w2339@student.hogent.be>
2017-11-02 14:38:31 +0100

    Add Script Serverconfig

---
+++ November2 - SQL/Scripts/0.ServerConfigNameIP.ps1
@@ -0,0 +1,9 @@
+﻿#Change Name
+Get-WmiObject win32_ComputerSystem
+$ComputerName = Get-WmiObject win32_ComputerSystem
+$name = "November2"
+$ComputerName.Rename($name)
+
+#IP configuration
+New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 172.18.2.5 -DefaultGateway 172.18.2.1 -PrefixLength 27
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses 172.18.2.67, 172.18.2.67
