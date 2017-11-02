cb4888e15a78ac0f21afffcfc38704b19afd70d3
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 19:39:53 +0100

    Update Script.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
@@ -2,6 +2,11 @@
 #Rename-computer -newname ns1
 #Restart-computer
 
+#IP-adres en default gateway wijzigen
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.1.2" -PrefixLength 24 -DefaultGateway "192.168.1.1"
+#DNS Server wijzigen
+Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses 192.168.0.1, 192.168.0.2
+
 #Firewall uitschakelen
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
