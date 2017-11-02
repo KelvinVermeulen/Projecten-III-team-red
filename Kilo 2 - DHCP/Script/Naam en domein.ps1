ee45313ec3634c48b0e608107757315e52c789bb
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 20:34:11 +0100

    Update Naam en domein.ps1

--- Kilo 2 - DHCP/Script/Naam en domein.ps1
+++ Kilo 2 - DHCP/Script/Naam en domein.ps1
@@ -1,7 +1,7 @@
 #IP-adres en default gateway wijzigen
 New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.1.3" -PrefixLength 24 -DefaultGateway "192.168.1.2"
 #DNS Server wijzigen
-Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses 192.168.1.2
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 192.168.1.2
 
 $cred = New-Object System.Management.Automation.PSCredential("red.local\Administrator",(ConvertTo-SecureString "Aa12345" -AsPlainText -Force))
 Add-Computer -DomainName red.local -Credential $cred -Restart
