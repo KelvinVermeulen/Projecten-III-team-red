681b8e82b79b57ab323a938c08972a5d4fdbefe1
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 18:42:47 +0100

    Update Script.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
@@ -73,6 +73,6 @@ New-GPLink -Name FileServer -Target "OU=Ontwikkeling,OU=RED,DC=red,dc=local"
 New-GPLink -Name FileServer -Target "OU=Directie,OU=RED,DC=red,dc=local"
 
 #Policies importeren
-Import-GPO -BackupID "058EA888-9327-48D6-98A4-DCA8FF5ACC64" -Path "" -TargetName "Netwerkadapter"
-Import-GPO -BackupID "C1C534A3-1039-42B5-9C3A-CBFBD710B182" -Path "" -TargetName "ControlPanelGameLink"
-Import-GPO -BackupID "7F004945-6DF1-4DD0-9E1D-2F2977A22CB3" -Path "" -TargetName "FileServer"
+Import-GPO -BackupID "058EA888-9327-48D6-98A4-DCA8FF5ACC64" -Path "F:\backup\" -TargetName "Netwerkadapter"
+Import-GPO -BackupID "C1C534A3-1039-42B5-9C3A-CBFBD710B182" -Path "F:\backup\" -TargetName "ControlPanelGameLink"
+Import-GPO -BackupID "7F004945-6DF1-4DD0-9E1D-2F2977A22CB3" -Path "F:\backup\" -TargetName "FileServer"
