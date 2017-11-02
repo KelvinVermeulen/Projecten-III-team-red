b9b0e31a81aba7d847a1cc5775526abc53ed5903
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 18:46:40 +0100

    Update DCenDNS-Naam wijzigen.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/DCenDNS-Naam wijzigen.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/DCenDNS-Naam wijzigen.ps1
@@ -2,6 +2,9 @@
 #Rename-computer -newname ns1
 #Restart-computer
 
+#Firewall uitschakelen
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
 #DC en DNS installeren en domeinnaam instellen
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "red.local" -SafeModeAdministratorPassword:(ConvertTo-SecureString -String "Aa12345" -AsPlainText -Force) -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainNetbiosName "RED" -ForestMode "Win2012R2" -InstallDns: $true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion: $false -SysvolPath "C:\Windows\SYSVOL" -Force: $true
