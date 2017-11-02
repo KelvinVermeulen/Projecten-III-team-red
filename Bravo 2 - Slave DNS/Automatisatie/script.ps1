86474a392b9d14bc2dfc19057573d5afe4155421
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 19:29:45 +0100

    Update script.ps1

--- Bravo 2 - Slave DNS/Automatisatie/script.ps1
+++ Bravo 2 - Slave DNS/Automatisatie/script.ps1
@@ -6,6 +6,4 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 #DC en DNS installeren en domeinnaam instellen
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-
-
 Install-ADDSDomainController -CreateDNSDelegation:$false -DatabasePath 'C:\Windows\NTDS' -DomainName 'red.local' -InstallDns:$true -LogPath 'C:\Windows\NTDS' -NoGlobalCatalog:$false -SysvolPath 'C:\Windows\SYSVOL' -NoRebootOnCompletion:$true -Force:$true -Credential (New-Object System.Management.Automation.PSCredential("red.local\Administrator",(ConvertTo-SecureString "Aa12345" -AsPlainText -Force))) -SafeModeAdministratorPassword:(ConvertTo-SecureString -String "Aa12345" -AsPlainText -Force)
