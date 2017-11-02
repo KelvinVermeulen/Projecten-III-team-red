ae46345de9ffe5bb722ad360476e56781992bf09
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-02 16:40:15 +0100

    Update Script.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
@@ -5,17 +5,18 @@ Restart-computer
 #DC en DNS installeren en domeinnaam instellen
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest
+-DomainName "red.local"
+-SafeModeAdministratorPassword:(ConvertTo-SecureString -String "Aa12345" -AsPlainText -Force)
 -CreateDnsDelegation:$true
--DatabasePath “C:\Windows\NTDS”
--DomainMode “Win2012R2”
--DomainName “red.local”
--DomainNetbiosName “RED”
--ForestMode “Win2012R2”
--InstallDns:$true
--LogPath “C:\Windows\NTDS”
--NoRebootOnCompletion:$false
--SysvolPath “C:\Windows\SYSVOL”
--Force:$true
+-DatabasePath "C:\Windows\NTDS"
+-DomainMode "Win2012R2"
+-DomainNetbiosName "RED"
+-ForestMode "Win2012R2"
+-InstallDns: $true
+-LogPath "C:\Windows\NTDS"
+-NoRebootOnCompletion: $false
+-SysvolPath "C:\Windows\SYSVOL"
+-Force: $true
 
 #OU aanmaken
 New-ADOrganizationalUnit -Name "RED" ;
