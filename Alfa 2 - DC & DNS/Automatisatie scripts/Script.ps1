7ad03c6d1e47f0445f18c5a761236151c036dd49
Hilmi Emre Bayat <hilmi_emre@hotmail.com>
2017-11-02 18:05:50 +0100

    Update

--- Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/Script.ps1
@@ -67,12 +67,12 @@ New-GPLink -Name Netwerkadapter -Target "OU=IT Administratie,OU=RED,DC=red,dc=lo
 
 #Groep policies aanmaken en linken met OU's
 new-gpo -name FileServer | new-gplink -target "OU=Administratie,OU=RED,DC=red,dc=local"
-New-GPLink -Name ControlPanelGameLink -Target "OU=Verkoop,OU=RED,DC=red,dc=local"
-New-GPLink -Name ControlPanelGameLink -Target "OU=IT Administratie,OU=RED,DC=red,dc=local"
-New-GPLink -Name ControlPanelGameLink -Target "OU=Ontwikkeling,OU=RED,DC=red,dc=local"
-New-GPLink -Name ControlPanelGameLink -Target "OU=Directie,OU=RED,DC=red,dc=local"
+New-GPLink -Name FileServer -Target "OU=Verkoop,OU=RED,DC=red,dc=local"
+New-GPLink -Name FileServer -Target "OU=IT Administratie,OU=RED,DC=red,dc=local"
+New-GPLink -Name FileServer -Target "OU=Ontwikkeling,OU=RED,DC=red,dc=local"
+New-GPLink -Name FileServer -Target "OU=Directie,OU=RED,DC=red,dc=local"
 
 #Policies importeren
 Import-GPO -BackupID "058EA888-9327-48D6-98A4-DCA8FF5ACC64" -Path "" -TargetName "Netwerkadapter"
 Import-GPO -BackupID "C1C534A3-1039-42B5-9C3A-CBFBD710B182" -Path "" -TargetName "ControlPanelGameLink"
-Import-GPO -BackupID "9323281F-E538-4562-9C02-A4533716C171" -Path "" -TargetName "FileServer"
+Import-GPO -BackupID "7F004945-6DF1-4DD0-9E1D-2F2977A22CB3" -Path "" -TargetName "FileServer"
