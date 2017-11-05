646a6f7998651bb2027ca52c705c1179bacb81fd
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-05 13:07:49 +0100

    Update script.ps1

--- Bravo 2 - Slave DNS/Automatisatie/script.ps1
+++ Bravo 2 - Slave DNS/Automatisatie/script.ps1
@@ -1,10 +1,10 @@
 #IP-adres en default gateway wijzigen
-New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.1.2" -PrefixLength 24 -DefaultGateway "192.168.1.1"
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "172.18.0.3" -PrefixLength 24 -DefaultGateway "172.18.0.1"
 #DNS Server wijzigen
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 192.168.1.1
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 172.18.0.2
 
 #Computer naam wijzigen --> Niet nodig waneer je de Windows Server automatisch laat installeren met autoUnattend.xml
-Rename-computer -newname ns2
+#Rename-computer -newname ns2
 
 #Firewall uitschakelen
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
