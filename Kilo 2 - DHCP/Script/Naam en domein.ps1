691cb12dd1a753d44273f71d3fcfb36236034b64
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-06 11:58:35 +0100

    Update Naam en domein.ps1

--- Kilo 2 - DHCP/Script/Naam en domein.ps1
+++ Kilo 2 - DHCP/Script/Naam en domein.ps1
@@ -3,6 +3,9 @@ New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "172.18.0.6" -PrefixLengt
 #DNS Server wijzigen
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 172.18.0.2
 
+#Firewall uitschakelen
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
 $cred = New-Object System.Management.Automation.PSCredential("red.local\Administrator",(ConvertTo-SecureString "Aa12345" -AsPlainText -Force))
 Add-Computer -DomainName red.local -Credential $cred
 
