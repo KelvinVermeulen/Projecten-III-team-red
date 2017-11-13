98691c8962452eec0389bfb42860897c5285355c
Cedric Van Dorpe <cedric.vandorpe.u8766@student.hogent.be>
2017-11-13 16:45:53 +0100

    share script gemaakt

--- Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
+++ Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
@@ -1,1 +1,9 @@
 new-smbshare -name VerkoopData -Path: D:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Verkoop"
+new-smbshare -name OntwikkelingData -Path: E:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Ontwikkeling"
+new-smbshare -name ITData -Path: F:\ -EncryptData $True -FullAccess "IT Administratie"
+new-smbshare -name DirData -Path: G:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Directie"
+new-smbshare -name AdminData -Path: H:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Administratie"
+new-smbshare -name HomeDirs -Path: Y:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "everyone"
+new-smbshare -name ProfileDirs -Path: Z:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "everyone"
+new-smbshare -name ShareVerkoop -Path: D:\ -EncryptData $True -FullAccess "IT Administratie" -ReadAccess "Ontwikkeling" -ChangeAccess "Verkoop"
+
