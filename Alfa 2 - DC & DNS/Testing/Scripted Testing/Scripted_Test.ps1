e49d98dc88dca3d6390d711fb2947475cf604191
Unknown <patrick-weyn@hotmail.be>
2017-11-14 12:45:09 +0100

    Alfa2: Initialisatie Test-script
    Checks windows version

---
+++ Alfa 2 - DC & DNS/Testing/Scripted Testing/Scripted_Test.ps1
@@ -0,0 +1,43 @@
+﻿#Variables ###########################################################################################
+$Windowsversion = "Microsoft Windows Server 2016 Standard"
+$successcount = 0
+$failedcount = 0
+
+echo "--------------------------------------------------------------"
+#Check if the correct version is installed ###########################################################
+echo "[$(Get-Date -format 'hh:mm:ss')] Checking windows server version:"
+if (
+(Get-WmiObject -class Win32_OperatingSystem).Caption |
+ Select-String -Pattern ${Windowsversion} -Quiet)
+{
+    echo "[$(Get-Date -format 'hh:mm:ss')][OK] Windows Version"
+    $successcount +=1
+}
+Else
+{
+    echo "[$(Get-Date -format 'hh:mm:ss')][FAILED] Windows Version"
+    Write-Host "[$(Get-Date -format 'hh:mm:ss')][ERROR]The installed OS is $((Get-WmiObject -class Win32_OperatingSystem).Caption)." -ForegroundColor Red
+    $failedcount +=1
+}
+
+echo "--------------------------------------------------------------"
+#Check if the correct version is installed ###########################################################
+echo "[$(Get-Date -format 'hh:mm:ss')] Checking the Network Configuration:"
+if (
+(Get-WmiObject -class Win32_OperatingSystem).Caption |
+ Select-String -Pattern ${Windowsversion} -Quiet)
+{
+    echo "[$(Get-Date -format 'hh:mm:ss')][OK] Windows Version"
+    $successcount +=1
+}
+Else
+{
+    echo "[$(Get-Date -format 'hh:mm:ss')][FAILED] Windows Version"
+    Write-Host "[$(Get-Date -format 'hh:mm:ss')][ERROR]The installed OS is $((Get-WmiObject -class Win32_OperatingSystem).Caption)." -ForegroundColor Red
+    $failedcount +=1
+}
+
+echo "--------------------------------------------------------------"
+
+echo "--------------------------------------------------------------"
+echo "SUCCESFUL = ${successcount} FAILED = ${failedcount}"
