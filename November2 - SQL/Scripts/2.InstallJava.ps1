3da475517ef304cb584b5aae51ba43e19e1f53a7
Benjamin <benjamin.vanlierde.w2339@student.hogent.be>
2017-11-16 14:56:28 +0100

    Added DotNetCore Install

--- November2 - SQL/Scripts/2.InstallJava.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Java.exe moet via bootable usb meegegeven worden.
-Write-Host 'Java installeren'
-Start-Process -FilePath "C:\Users\Administrator\Documents\Installattie\JreJava.exe" -ArgumentList '/s' -wait -PassThru
