e744fca63b2a2c63751711d54cb159fabbdfc997
Cedric Van Dorpe <cedric.vandorpe.u8766@student.hogent.be>
2017-11-20 17:12:12 +0100

    aanpassing script

--- Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
+++ Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
@@ -1,3 +1,6 @@
+
+#shares & permissions
+
 new-smbshare -name VerkoopData -Path: D:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Verkoop"
 new-smbshare -name OntwikkelingData -Path: E:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "Ontwikkeling"
 new-smbshare -name ITData -Path: F:\ -EncryptData $True -FullAccess "IT Administratie"
@@ -6,4 +9,14 @@ new-smbshare -name AdminData -Path: H:\ -EncryptData $True -FullAccess "IT Admin
 new-smbshare -name HomeDirs -Path: Y:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "everyone"
 new-smbshare -name ProfileDirs -Path: Z:\ -EncryptData $True -FullAccess "IT Administratie" -ChangeAccess "everyone"
 new-smbshare -name ShareVerkoop -Path: D:\ -EncryptData $True -FullAccess "IT Administratie" -ReadAccess "Ontwikkeling" -ChangeAccess "Verkoop"
+
+# quotas
+
+Set-FsrmQuota -Path "D:\" -Size 100MB
+Set-FsrmQuota -Path "G:\" -Size 100MB
+Set-FsrmQuota -Path "H:\" -Size 100MB 
+
+Set-FsrmQuota -Path "E:\" -Size 200MB
+Set-FsrmQuota -Path "F:\" -Size 200MB
+
 
