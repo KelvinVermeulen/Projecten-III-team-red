9d3d184fdc8a2e5226f8a9b0d91737f9224248f3
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-11-20 16:41:33 +0100

    Update Users-GPO.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/Users-GPO.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/Users-GPO.ps1
@@ -62,11 +62,11 @@ New-GPLink -Name Netwerkadapter -Target "OU=IT Administratie,OU=RED,DC=red,dc=lo
 new-gpo -name FileServer | new-gplink -target "DC=red,dc=local"
 
 #Groep policies aanmaken en linken met OU's
-new-gpo -name SQL poorten voor SCCM2012R2 | new-gplink -target "DC=red,dc=local"
+new-gpo -name SQLpoortenVoorSCCM2012R2 | new-gplink -target "DC=red,dc=local"
 
 
 #Policies importeren
 Import-GPO -BackupID "058EA888-9327-48D6-98A4-DCA8FF5ACC64" -Path "D:\backup\" -TargetName "Netwerkadapter"
 Import-GPO -BackupID "C1C534A3-1039-42B5-9C3A-CBFBD710B182" -Path "D:\backup\" -TargetName "ControlPanelGameLink"
 Import-GPO -BackupID "A6C24491-2822-4A58-B11F-605982B160D5" -Path "D:\backup\" -TargetName "FileServer"
-Import-GPO -BackupID "09068053-EC76-4B2A-B9FE-0BC6DD0290D1" -Path "D:\backup\" -TargetName "SQL poorten voor SCCM2012R2"
+Import-GPO -BackupID "09068053-EC76-4B2A-B9FE-0BC6DD0290D1" -Path "D:\backup\" -TargetName "SQLpoortenVoorSCCM2012R2"
