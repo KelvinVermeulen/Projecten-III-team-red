a62281385bc75a92cfcb1110436b8a9c547bb746
BRAM VANDENBOSSCHE <BRAM.VANDENBOSSCHE.V3613@STUDENT.HOGENT.BE>
2017-11-27 12:14:27 +0100

    script partitioning update

--- Lima 2 - Interne File Server/Scripts/Partitioning.ps1
+++ Lima 2 - Interne File Server/Scripts/Partitioning.ps1
@@ -3,40 +3,34 @@
 #Disk 0
 Initialize-Disk 0
 
-New-Partition -DiskNumber 0 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 0 -Size 20GB -DriveLetter D 
 Format-Volume -DriveLetter  -FileSystem NTFS
 Set-Volume -DriveLetter E -NewFileSystemLabel "VerkoopData"
-Set-Partition -DriveLetter XXX -NewDriveLetter D
 
-New-Partition -DiskNumber 0 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 0 -Size 20GB -DriveLetter E
 Format-Volume -DriveLetter  -FileSystem NTFS
 Set-Volume -DriveLetter E -NewFileSystemLabel "OntwikkelingData"
-Set-Partition -DriveLetter XXX -NewDriveLetter E
 
 #Disk 1
 Initialize-Disk 1
 
-New-Partition -DiskNumber 1 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 1 -Size 20GB -DriveLetter F
 Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter E -NewFileSystemLabel "ITData"
-Set-Partition -DriveLetter #XXX -NewDriveLetter F
 
-New-Partition -DiskNumber 1 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 1 -Size 20GB -DriveLetter G
 Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter E -NewFileSystemLabel "DirData"
-Set-Partition -DriveLetter #XXX -NewDriveLetter G
 
-New-Partition -DiskNumber 1 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 1 -Size 20GB -DriveLetter H 
 Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter  -NewFileSystemLabel "AdminData"
-Set-Partition -DriveLetter #XXX -NewDriveLetter H
 
-New-Partition -DiskNumber 1 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 1 -Size 20GB -DriveLetter Y 
 Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter  -NewFileSystemLabel "HomeDirs"
-Set-Partition -DriveLetter #XXX -NewDriveLetter Y
 
-New-Partition -DiskNumber 1 -Size 20GB -AssignDriverLetter 
+New-Partition -DiskNumber 1 -Size 20GB -DriveLetter Z
 Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter  -NewFileSystemLabel "ProfileDirs"
-Set-Partition -DriveLetter #XXX -NewDriveLetter Z
+
