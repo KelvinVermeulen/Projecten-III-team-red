4316098857024ade4f3b4d8d234d03e2e6cc939a
BRAM VANDENBOSSCHE <BRAM.VANDENBOSSCHE.V3613@STUDENT.HOGENT.BE>
2017-11-27 12:21:33 +0100

    partitioning script update

--- Lima 2 - Interne File Server/Scripts/Partitioning.ps1
+++ Lima 2 - Interne File Server/Scripts/Partitioning.ps1
@@ -4,33 +4,33 @@
 Initialize-Disk 0
 
 New-Partition -DiskNumber 0 -Size 20GB -DriveLetter D 
-Format-Volume -DriveLetter  -FileSystem NTFS
-Set-Volume -DriveLetter E -NewFileSystemLabel "VerkoopData"
+Format-Volume -DriveLetter D -FileSystem NTFS
+Set-Volume -DriveLetter D -NewFileSystemLabel "VerkoopData"
 
 New-Partition -DiskNumber 0 -Size 20GB -DriveLetter E
-Format-Volume -DriveLetter  -FileSystem NTFS
+Format-Volume -DriveLetter E -FileSystem NTFS
 Set-Volume -DriveLetter E -NewFileSystemLabel "OntwikkelingData"
 
 #Disk 1
 Initialize-Disk 1
 
 New-Partition -DiskNumber 1 -Size 20GB -DriveLetter F
-Format-Volume -DriveLetter E -FileSystem NTFS
-Set-Volume -DriveLetter E -NewFileSystemLabel "ITData"
+Format-Volume -DriveLetter F -FileSystem NTFS
+Set-Volume -DriveLetter F -NewFileSystemLabel "ITData"
 
 New-Partition -DiskNumber 1 -Size 20GB -DriveLetter G
-Format-Volume -DriveLetter E -FileSystem NTFS
-Set-Volume -DriveLetter E -NewFileSystemLabel "DirData"
+Format-Volume -DriveLetter G -FileSystem NTFS
+Set-Volume -DriveLetter G -NewFileSystemLabel "DirData"
 
 New-Partition -DiskNumber 1 -Size 20GB -DriveLetter H 
-Format-Volume -DriveLetter E -FileSystem NTFS
-Set-Volume -DriveLetter  -NewFileSystemLabel "AdminData"
+Format-Volume -DriveLetter H -FileSystem NTFS
+Set-Volume -DriveLetter H -NewFileSystemLabel "AdminData"
 
 New-Partition -DiskNumber 1 -Size 20GB -DriveLetter Y 
-Format-Volume -DriveLetter E -FileSystem NTFS
-Set-Volume -DriveLetter  -NewFileSystemLabel "HomeDirs"
+Format-Volume -DriveLetter Y -FileSystem NTFS
+Set-Volume -DriveLetter Y -NewFileSystemLabel "HomeDirs"
 
 New-Partition -DiskNumber 1 -Size 20GB -DriveLetter Z
-Format-Volume -DriveLetter E -FileSystem NTFS
-Set-Volume -DriveLetter  -NewFileSystemLabel "ProfileDirs"
+Format-Volume -DriveLetter Z -FileSystem NTFS
+Set-Volume -DriveLetter Z -NewFileSystemLabel "ProfileDirs"
 
