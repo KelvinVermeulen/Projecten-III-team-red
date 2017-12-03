1ad2bec1530b528a229211a7f6564dba41398c60
BRAM VANDENBOSSCHE <BRAM.VANDENBOSSCHE.V3613@STUDENT.HOGENT.BE>
2017-12-03 20:09:25 +0100

    update script DFS

--- Lima 2 - Interne File Server/Scripts/DFS configuration.ps1
+++ Lima 2 - Interne File Server/Scripts/DFS configuration.ps1
@@ -6,10 +6,10 @@
 $folders = (‘C:\dfsroots\files’,’D:\ShareVerkoop’)  mkdir -path $folders
 #Make share
 $folders | ForEach-Object {$sharename = (Get-Item $_).name; New-SMBShare -Name $shareName -Path $_ -FullAccess Everyone}
-#
+#DFNSroot
 New-DfsnRoot -Path \\red.local\ShareVerkoop -TargetPath \\lima2\ShareVerkoop -Type DomainV2
 
-# Add folder target to namespace
+#Add folder target to namespace
 $folders | Where-Object {$_ -like "*ShareVerkoop*"} | ForEach-Object {$name = (Get-Item $_).name; 
 $DfsPath = (‘\\red.local\ShareVerkoop\’ + $name); 
 $targetPath = (‘\\lima2\’ + $name);
