b04bd38f54ae25fe0a64e0261145de8f949e0c3e
Antonio <antonio.pizarro.u1704@student.hogent.be>
2017-12-03 16:33:22 +0100

    Script configuratie Exchange - mailbox

---
+++ Charlie 2 - Microsoft exchange/Scripts/exchange2013_config.ps1
@@ -0,0 +1,10 @@
+﻿#Deze script moet uitgevoerd worden in de Exchange Management Shell
+#Maar kan ook in de gewone Powershell uitgevoerd worden als volgende SnapIn geïnstalleerd wordt
+
+#Add-PSSnapin microsoft-exhcange.management.powershell.snapin
+#Add-PSSnapin microsoft-exhcange.management.powershell.setup
+#Add-PSSnapin microsoft-exhcange.management.powershell.support
+
+#Elke bestaande user krijgt een mailbox.
+
+Get-User | Where-Object {$_.RecipientType -eq "User"} | ForEach-Object {Enable-Mailbox -Identity $_.Name}
