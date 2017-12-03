f41ac6b286b34b047330f1e7141f51b8192d2e3b
VanDorpeCedric <cedric.vandorpe.u8766@student.hogent.be>
2017-12-03 20:47:08 +0100

    Update SharesQuotasAndShadowCopy.ps1

--- Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
+++ Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
@@ -18,10 +18,10 @@ $Threshold = New-FsrmQuotaThreshold -Percentage 90 -Action $action
 # example thingy : New-FsrmQuota -Path "C:\Shares" -Size 128MB -Threshold $Threshold -Softlimit (softlimit does not enforce the threshold, but just logs it)
 
 New-FsrmQuota -Path "D:\" -Description "VerkoopData Quota" -Size 100MB -Threshhold $Threshold
-New-FsrmQuota -Path "G:\" -Size 100MB
-New-FsrmQuota -Path "H:\" -Size 100MB   
+New-FsrmQuota -Path "G:\" -Description "DirData Quota" -Size 100MB -Threshhold $Threshold
+New-FsrmQuota -Path "H:\" -Description "AdminData Quota" -Size 100MB -Threshhold $Threshold
 
-New-FsrmQuota -Path "E:\" -Size 200MB
-New-FsrmQuota -Path "F:\" -Size 200MB
+New-FsrmQuota -Path "E:\" -Description "OntwikkelingData Quota" -Size 200MB -Threshhold $Threshold
+New-FsrmQuota -Path "F:\" -Description "ITData Quota" -Size 200MB -Threshhold $Threshold
 
 # shadowcopy
