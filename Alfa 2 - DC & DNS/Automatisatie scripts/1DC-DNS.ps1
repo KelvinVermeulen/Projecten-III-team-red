042ad3f0d76affdee3bf43507d2a51f8cc4ac2be
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-12-04 15:06:52 +0100

    Update 1DC-DNS.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/1DC-DNS.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/1DC-DNS.ps1
@@ -5,6 +5,7 @@
 New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "172.18.0.2" -PrefixLength 24 -DefaultGateway "172.18.0.1"
 #DNS Server wijzigen
 #Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 192.168.1.1
+Get-NetAdapterBinding -ComponentID 'ms_tcpip6' | Disable-NetAdapterBinding
 
 #Firewall uitschakelen
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
