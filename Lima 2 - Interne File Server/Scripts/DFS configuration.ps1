db0e92fd3d5bf656e8c9f117ee359ca2658047b0
BRAM VANDENBOSSCHE <BRAM.VANDENBOSSCHE.V3613@STUDENT.HOGENT.BE>
2017-12-04 16:50:55 +0100

    script dfs

--- Lima 2 - Interne File Server/Scripts/DFS configuration.ps1
+++ Lima 2 - Interne File Server/Scripts/DFS configuration.ps1
@@ -6,9 +6,7 @@
 $folders = 'C:\dfsroots\files','D:\testShare' 
 mkdir -path $folders
 #Make share
-$sharename = testShare; 
-$folders | ForEach-Object {$sharename = (Get-Item $_).name; 
-New-SMBShare -Name $shareName -Path "D:\testShare" -ReadAcces "Ontwikkeling" -ChangeAcces "Verkoop" -FullAcces "IT Administratie"}
+New-SMBShare -Name testShare -Path "D:\testShare" -ReadAccess "Ontwikkeling" -ChangeAccess "Verkoop" -FullAccess "IT Administratie"}
 #DFNSroot
 New-DfsnRoot -Path \\red.local\testShare -TargetPath \\lima2\testShare -Type DomainV2
 
@@ -16,4 +14,6 @@ New-DfsnRoot -Path \\red.local\testShare -TargetPath \\lima2\testShare -Type Dom
 $DfsPath = \\red.local\files\testShare 
 $targetPath = \\lima2\testShare
 New-DfsnFolderTarget -Path $DfsPath -TargetPath $targetPath
+
+
 
