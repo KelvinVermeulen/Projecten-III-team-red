ba327101507756729f85197d60c7c953659a4a39
Cedric Van Dorpe <cedric.vandorpe.u8766@student.hogent.be>
2017-12-04 15:19:01 +0100

    shares script aangepast

--- Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
+++ Lima 2 - Interne File Server/Scripts/SharesQuotasAndShadowCopy.ps1
@@ -8,21 +8,22 @@ new-smbshare -name DirData -Path: G:\ -EncryptData $False -FullAccess "IT Admini
 new-smbshare -name AdminData -Path: H:\ -EncryptData $False -FullAccess "IT Administratie" -ChangeAccess "Administratie"
 new-smbshare -name HomeDirs -Path: Y:\ -EncryptData $False -FullAccess "IT Administratie" -ChangeAccess "everyone"
 new-smbshare -name ProfileDirs -Path: Z:\ -EncryptData $False -FullAccess "IT Administratie" -ChangeAccess "everyone"
-new-smbshare -name ShareVerkoop -Path: D:\ -EncryptData $False -FullAccess "IT Administratie" -ReadAccess "Ontwikkeling" -ChangeAccess "Verkoop"
+#new-smbshare -name ShareVerkoop -Path: D:\ -EncryptData $False -FullAccess "IT Administratie" -ReadAccess "Ontwikkeling" -ChangeAccess "Verkoop"
  
 # quotas
 # http://techgenix.com/powershell-file-management-part3/
 
+
+# example thingy : New-FsrmQuota -Path "C:\Shares" -Size 128MB -Threshold $Threshold -Softlimit (softlimit does not enforce the threshold, but just logs it)
 $Action = New-FsrmAction -Type Command -Command "c:\windows\system32\cmd.exe" -CommandParameters "echo  >> c:\log.txt" -ShouldLogError
 $Threshold = New-FsrmQuotaThreshold -Percentage 90 -Action $action
-# example thingy : New-FsrmQuota -Path "C:\Shares" -Size 128MB -Threshold $Threshold -Softlimit (softlimit does not enforce the threshold, but just logs it)
 
-New-FsrmQuota -Path "D:\" -Description "VerkoopData Quota" -Size 100MB -Threshhold $Threshold
-New-FsrmQuota -Path "G:\" -Description "DirData Quota" -Size 100MB -Threshhold $Threshold
-New-FsrmQuota -Path "H:\" -Description "AdminData Quota" -Size 100MB -Threshhold $Threshold
+New-FsrmQuota -Path "D:\" -Description "VerkoopData Quota" -Size 100MB -Threshold $Threshold
+New-FsrmQuota -Path "G:\" -Description "DirData Quota" -Size 100MB -Threshold $Threshold
+New-FsrmQuota -Path "H:\" -Description "AdminData Quota" -Size 100MB -Threshold $Threshold
 
-New-FsrmQuota -Path "E:\" -Description "OntwikkelingData Quota" -Size 200MB -Threshhold $Threshold
-New-FsrmQuota -Path "F:\" -Description "ITData Quota" -Size 200MB -Threshhold $Threshold
+New-FsrmQuota -Path "E:\" -Description "OntwikkelingData Quota" -Size 200MB -Threshold $Threshold
+New-FsrmQuota -Path "F:\" -Description "ITData Quota" -Size 200MB -Threshold $Threshold
 
 # shadowcopy
 
