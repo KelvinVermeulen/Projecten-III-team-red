527bea00971691c550a79002f8b79e59ca2dccc8
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-12-11 16:15:08 +0100

    Update 4GPO.ps1

--- Alfa 2 - DC & DNS/Automatisatie scripts/4GPO.ps1
+++ Alfa 2 - DC & DNS/Automatisatie scripts/4GPO.ps1
@@ -24,4 +24,4 @@ Import-GPO -BackupID "058EA888-9327-48D6-98A4-DCA8FF5ACC64" -Path "D:\backup\" -
 Import-GPO -BackupID "C1C534A3-1039-42B5-9C3A-CBFBD710B182" -Path "D:\backup\" -TargetName "ControlPanelGameLink"
 Import-GPO -BackupID "0C8FA57B-76C8-4847-8C89-7B90911FBC12" -Path "D:\backup\" -TargetName "FileServer"
 Import-GPO -BackupID "09068053-EC76-4B2A-B9FE-0BC6DD0290D1" -Path "D:\backup\" -TargetName "SQLpoortenVoorSCCM2012R2"
-Import-GPO -BackupID "1C5EC1E6-8650-4742-B1E2-C163DE190439" -Path "D:\backup\" -TargetName "ActiveScripting"
+Import-GPO -BackupID "9C15F84F-D5DA-4BB6-ACE8-982B6DD9E7BA" -Path "D:\backup\" -TargetName "ActiveScripting"
