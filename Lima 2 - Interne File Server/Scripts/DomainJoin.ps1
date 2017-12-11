a8d21ae7e3871319ee17a133775fbb5b5697a329
BramVandenbossche <bramvandenbossche@hotmail.com>
2017-12-11 15:28:39 +0100

    Update DomainJoin.ps1

--- Lima 2 - Interne File Server/Scripts/DomainJoin.ps1
+++ Lima 2 - Interne File Server/Scripts/DomainJoin.ps1
@@ -3,3 +3,4 @@ $password = "Aa12345" | ConvertTo-SecureString -asPlainText -Force
 $username = "$domain\Administrator" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential
+Restart-Computer
