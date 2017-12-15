af16b520e5c6487554e65585296fbd46ea8e0ffe
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-12-15 10:57:24 +0100

    Update Naam en domein.ps1

--- Kilo 2 - DHCP/Script/Naam en domein.ps1
+++ Kilo 2 - DHCP/Script/Naam en domein.ps1
@@ -1,7 +1,7 @@
 #IP-adres en default gateway wijzigen
 New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "172.18.2.2" -PrefixLength 27 -DefaultGateway "172.18.2.1"
 #DNS Server wijzigen
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "172.18.2.67","172.18.2.67"
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "172.18.2.67","172.18.2.68"
 
 #Firewall uitschakelen
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
